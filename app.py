78add23683deb995c5fa3d59dbf6511752ed35a9
Alonso Silva Allende <alonsosilva@gmaiil.com>
2020-08-12 21:27:32 +0200

    Add new confirmed cases

--- app.py
+++ app.py
@@ -54,3 +54,54 @@ chart = (
 st.altair_chart(chart)
 
 st.markdown("*El 17 de junio, se añadieron 31.422 casos confirmados debido a revisiones en el sistema de epivigilia y las fuentes de datos ([ver noticia](https://www.biobiochile.cl/noticias/nacional/chile/2020/06/16/minsal-anade-otros-31-412-contagios-covid-19-no-estaban-informados-total-supera-los-215-mil.shtml)).")
+
+st.title("Evolución de nuevos casos confirmados por región")
+st.markdown("*Advertencia*: El número de casos confirmados no representa exactamente el número de casos/contagios reales. Estos últimos valores no son conocidos por límites de testeo.")
+df = get_data()
+
+df = df.T
+
+df[pd.to_datetime("2020-03-02")] = [0 for i in range(len(df.index))]
+
+df = df.T
+
+df = df.sort_index()
+
+df = df.diff()
+
+#region = st.multiselect(
+#	"Elegir regiones", list(df.columns), ["Metropolitana", "Valparaíso"]
+#)
+
+#start_date = st.date_input('Fecha de inicio', df.index[0])
+#end_date = st.date_input('Fecha de termino', df.index[-1])
+#if start_date < end_date:
+#    st.success('Fecha de inicio: `%s`\n\nFecha de termino: `%s`' % (start_date, end_date))
+#else:
+#    st.error('Error: La fecha de término debe ser después de la fecha de inicio.')
+
+df = df[region].loc[start_date:end_date]
+st.write("### Nuevos casos confirmados", df.T)
+df = df.reset_index()
+df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name="nuevos casos confirmados")#.rename(columns={"Region": "fecha"})
+
+chart = (
+    alt.Chart(df)
+    .mark_line()
+    .encode(
+        x="fecha:T",
+        y=alt.Y("nuevos casos confirmados", stack=None),
+		tooltip = ['nuevos casos confirmados'],
+        color="Región:N",
+    ).properties(
+    	title='Nuevos casos confirmados*',
+		height=600,
+    	width=600
+	)
+)
+
+st.altair_chart(chart)
+
+st.markdown("*El 17 de junio, se añadieron 31.422 casos confirmados debido a revisiones en el sistema de epivigilia y las fuentes de datos ([ver noticia](https://www.biobiochile.cl/noticias/nacional/chile/2020/06/16/minsal-anade-otros-31-412-contagios-covid-19-no-estaban-informados-total-supera-los-215-mil.shtml)).")
+
+
