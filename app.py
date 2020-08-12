51ec709b98f5e4e0b2050cc4650f8cf773fb9d04
Alonso Silva Allende <alonsosilva@gmaiil.com>
2020-08-12 18:49:19 +0200

    Add time slider

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import altair as alt
 import streamlit as st
+import datetime
 
 st.title("Evolución del total de casos confirmados acumulados por región")
 st.markdown("*Advertencia*: El número de casos confirmados no representa exactamente el número de casos/contagios reales. Estos últimos valores no son conocidos por límites de testeo.")
@@ -11,7 +12,11 @@ st.markdown("Datos: [Ministerio de Ciencia](https://github.com/MinCiencia/Datos-
 def get_data():
 	URL = "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto3/CasosTotalesCumulativo_T.csv"
 	df = pd.read_csv(URL)
-	return df.set_index("Region")
+	df = df.rename(columns={"Region": "fecha"})
+	df["fecha"] = pd.to_datetime(df["fecha"])
+	df = df.set_index("fecha")
+	df = df.sort_index()
+	return df
 
 df = get_data()
 
@@ -19,10 +24,17 @@ region = st.multiselect(
 	"Elegir regiones", list(df.columns), ["Metropolitana", "Valparaíso"]
 )
 
-df = df[region]
+start_date = st.date_input('Fecha de inicio', df.index[0])
+end_date = st.date_input('Fecha de termino', df.index[-1])
+if start_date < end_date:
+    st.success('Fecha de inicio: `%s`\n\nFecha de termino: `%s`' % (start_date, end_date))
+else:
+    st.error('Error: La fecha de término debe ser después de la fecha de inicio.')
+
+df = df[region].loc[start_date:end_date]
 st.write("### Total de casos confirmados acumulados", df.T)
 df = df.reset_index()
-df = pd.melt(df, id_vars=["Region"], var_name="Región" , value_name="total casos confirmados").rename(columns={"Region": "fecha"})
+df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name="total casos confirmados")#.rename(columns={"Region": "fecha"})
 
 chart = (
     alt.Chart(df)
@@ -30,7 +42,7 @@ chart = (
     .encode(
         x="fecha:T",
         y=alt.Y("total casos confirmados", stack=None),
-		tooltip = ['fecha', 'total casos confirmados'],
+		tooltip = ['total casos confirmados'],
         color="Región:N",
     ).properties(
     	title='Total de casos confirmados acumulados*',
