dae78fc85f960165867926f0e080326ce8867035
Alonso Silva Allende <alonsosilva@gmaiil.com>
2020-08-13 15:43:42 +0200

    Add tasas de incidencia acumulada/instantanea

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import altair as alt
 import streamlit as st
 import datetime
 
-st.title("Evolución del total de casos confirmados acumulados por región")
-st.markdown("*Advertencia*: El número de casos confirmados no representa exactamente el número de casos/contagios reales. Estos últimos valores no son conocidos por límites de testeo.")
 st.markdown("Autor: [Alonso Silva](https://github.com/alonsosilvaallende)")
 st.markdown("Datos: [Ministerio de Ciencia](https://github.com/MinCiencia/Datos-COVID19)")
 
@@ -19,6 +17,34 @@ def get_data():
 	df = df.sort_index()
 	return df
 
+@st.cache
+def get_population():
+    URL = "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto7/PCR.csv"
+    df = pd.read_csv(URL)
+    df = df[["Region", "Poblacion"]]
+    df = df.set_index("Region")
+    return df
+
+def my_altair_plot(df, value_name, title):
+	df = df.reset_index()
+	df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name=value_name)
+	
+	chart = (
+	    alt.Chart(df)
+	    .mark_line()
+	    .encode(
+	        x="fecha:T",
+	        y=alt.Y(value_name, stack=None),
+			tooltip = [value_name],
+	        color="Región:N",
+	    ).properties(
+	    	title=title,
+			height=600,
+	    	width=600
+		)
+	)
+	return chart
+	
 df = get_data()
 
 region = st.multiselect(
@@ -26,40 +52,23 @@ region = st.multiselect(
 )
 
 start_date = st.date_input('Fecha de inicio', df.index[0])
-end_date = st.date_input('Fecha de termino', df.index[-1])
+end_date = st.date_input('Fecha de término', df.index[-1])
 
-if start_date < end_date:
-	pass
-    #st.success('Fecha de inicio: `%s`\n\nFecha de termino: `%s`' % (start_date, end_date))
-else:
+if start_date > end_date:
     st.error('Error: La fecha de término debe ser después de la fecha de inicio.')
 
 df = df[region].loc[start_date:end_date]
 df = df.sort_index(ascending=False)
 
+st.title("Evolución del total de casos confirmados acumulados por región")
+st.markdown("*Advertencia*: El número de casos confirmados no representa exactamente el número de casos/contagios reales. Estos últimos valores no son conocidos por límites de testeo.")
 show_df = st.checkbox("Mostrar Tabla 1")
 if show_df:
 	st.write(df)
 
-df = df.reset_index()
-df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name="total casos confirmados")
-
-chart = (
-    alt.Chart(df)
-    .mark_line()
-    .encode(
-        x="fecha:T",
-        y=alt.Y("total casos confirmados", stack=None),
-		tooltip = ['total casos confirmados'],
-        color="Región:N",
-    ).properties(
-    	title='Total de casos confirmados acumulados*',
-		height=600,
-    	width=600
-	)
-)
-
-st.altair_chart(chart+chart)
+title = "Total de casos confirmados acumulados"
+chart = my_altair_plot(df, "total casos confirmados", title)
+st.altair_chart(chart)
 
 st.markdown("*El 17 de junio, se añadieron 31.422 casos confirmados debido a revisiones en el sistema de epivigilia y las fuentes de datos ([ver noticia](https://www.biobiochile.cl/noticias/nacional/chile/2020/06/16/minsal-anade-otros-31-412-contagios-covid-19-no-estaban-informados-total-supera-los-215-mil.shtml)).")
 
@@ -74,17 +83,6 @@ df = df.T
 df = df.sort_index()
 df = df.diff()
 
-#region = st.multiselect(
-#	"Elegir regiones", list(df.columns), ["Metropolitana", "Valparaíso"]
-#)
-
-#start_date = st.date_input('Fecha de inicio', df.index[0])
-#end_date = st.date_input('Fecha de termino', df.index[-1])
-#if start_date < end_date:
-#    st.success('Fecha de inicio: `%s`\n\nFecha de termino: `%s`' % (start_date, end_date))
-#else:
-#    st.error('Error: La fecha de término debe ser después de la fecha de inicio.')
-
 df = df[region].loc[start_date:end_date]
 show_df_new = st.checkbox("Mostrar Tabla 2")
 if show_df_new:
@@ -94,28 +92,65 @@ pm = st.checkbox("Promedio móvil 7 días", True)
 if pm:
 	df = df.rolling(window=7).mean()
 
-df = df.reset_index()
-
-df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name="nuevos casos confirmados")#.rename(columns={"Region": "fecha"})
-
-
-chart = (
-    alt.Chart(df)
-    .mark_line()
-    .encode(
-        x="fecha:T",
-        y=alt.Y("nuevos casos confirmados", stack=None),
-		tooltip = ['nuevos casos confirmados'],
-        color="Región:N",
-    ).properties(
-    	title='Nuevos casos confirmados*',
-		height=600,
-    	width=600
-	)
-)
-
-st.altair_chart(chart + chart)
+title = "Nuevos casos confirmados"
+chart = my_altair_plot(df, "nuevos casos confirmados", title)
+st.altair_chart(chart)
 
 st.markdown("*El 17 de junio, se añadieron 31.422 casos confirmados debido a revisiones en el sistema de epivigilia y las fuentes de datos ([ver noticia](https://www.biobiochile.cl/noticias/nacional/chile/2020/06/16/minsal-anade-otros-31-412-contagios-covid-19-no-estaban-informados-total-supera-los-215-mil.shtml)).")
 
+st.title("Evolución del total de casos confirmados acumulados por 100.000 habitantes/tasa de incidencia acumulada por región")
+st.markdown("*Advertencia*: El número de casos confirmados no representa exactamente el número de casos/contagios reales. Estos últimos valores no son conocidos por límites de testeo. Además, los casos confirmados por 100.000 habitantes, dan mayores valores a regiones con pocos habitantes, por lo que pueden introducir sesgos en los datos.")
 
+df = get_data()
+pop = get_population()
+df = df.T
+pop = pop.reset_index().append({"Region": "Total", "Poblacion": pop["Poblacion"].sum()}, ignore_index=True)
+pop = pop.set_index("Region")
+df["Población"] = pop
+df = df.apply(lambda x: 100000*x[:-1]/x[-1], axis=1)
+df = df.T
+df = df[region].loc[start_date:end_date]
+df = df.sort_index(ascending=False)
+show_df = st.checkbox("Mostrar Tabla 3")
+if show_df:
+	st.write(df)
+
+title = "Total de casos confirmados acumulados por 100.000 habitantes"
+chart = my_altair_plot(df, "total casos confirmados", title)
+
+st.altair_chart(chart)
+
+st.markdown("*El 17 de junio, se añadieron 31.422 casos confirmados debido a revisiones en el sistema de epivigilia y las fuentes de datos ([ver noticia](https://www.biobiochile.cl/noticias/nacional/chile/2020/06/16/minsal-anade-otros-31-412-contagios-covid-19-no-estaban-informados-total-supera-los-215-mil.shtml)).")
+
+st.title("Evolución de nuevos casos confirmados por 100.000 habitantes/tasa de incidencia por región")
+st.markdown("*Advertencia*: El número de casos confirmados no representa exactamente el número de casos/contagios reales. Estos últimos valores no son conocidos por límites de testeo. Además, los casos confirmados por 100.000 habitantes, dan mayores valores a regiones con pocos habitantes, por lo que pueden introducir sesgos en los datos.")
+
+df = get_data()
+# Agregar el 2 de marzo para hacer el diff
+df = df.T
+df[pd.to_datetime("2020-03-02")] = [0 for i in range(len(df.index))]
+df = df.T
+df = df.sort_index()
+df = df.diff()
+pop = get_population()
+df = df.T
+pop = pop.reset_index().append({"Region": "Total", "Poblacion": pop["Poblacion"].sum()}, ignore_index=True)
+pop = pop.set_index("Region")
+df["Población"] = pop
+df = df.apply(lambda x: 100000*x[:-1]/x[-1], axis=1)
+df = df.T
+df = df[region].loc[start_date:end_date]
+df = df.sort_index(ascending=False)
+show_df = st.checkbox("Mostrar Tabla 4")
+if show_df:
+	st.write(df)
+pm = st.checkbox("Suavizar/promedio móvil 7 días", True)
+if pm:
+	df = df.rolling(window=7).mean()
+
+title = "Nuevos casos confirmados por 100.000 habitantes"
+chart = my_altair_plot(df, "número de casos confirmados", title)
+
+st.altair_chart(chart)
+
+st.markdown("*El 17 de junio, se añadieron 31.422 casos confirmados debido a revisiones en el sistema de epivigilia y las fuentes de datos ([ver noticia](https://www.biobiochile.cl/noticias/nacional/chile/2020/06/16/minsal-anade-otros-31-412-contagios-covid-19-no-estaban-informados-total-supera-los-215-mil.shtml)).")
