9b702aa7c62a2ee410ea9214dece1a413f95058a
Alonso Silva Allende <alonsosilva@gmaiil.com>
2020-08-14 12:10:32 +0200

    Add different kinds of plots

--- app.py
+++ app.py
@@ -22,14 +22,11 @@ def get_population():
     df = df.set_index("Region")
     return df
 
-def my_altair_plot(df, value_name, title):
+def my_altair_plot(df, value_name, title, option="Normal"):
 	df = df.reset_index()
 	df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name=value_name)
-	
 	chart = (
-	    alt.Chart(df)
-	    .mark_line()
-	    .encode(
+	    alt.Chart(df).mark_line().encode(
 	        x="fecha:T",
 	        y=alt.Y(value_name, stack=None),
 			tooltip = [value_name],
@@ -42,19 +39,55 @@ def my_altair_plot(df, value_name, title):
 	)
 	return chart
 	
+def my_percentage_plot(df, value_name, title):
+	df = df.reset_index()
+	df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name=value_name)
+	chart = (
+	    alt.Chart(df).mark_area().encode(
+	        x="fecha:T",
+	        y=alt.Y(value_name, axis=alt.Axis(format='%'), stack="normalize"),
+			tooltip = [value_name],
+	        color="Región:N",
+	    ).properties(
+	    	title=title,
+			height=600,
+	    	width=600
+		)
+	)
+	return chart
+
+def my_streamgraph_plot(df, value_name, title):
+	df = df.reset_index()
+	df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name=value_name)
+	chart = (
+	    alt.Chart(df).mark_area().encode(
+	        x="fecha:T",
+	        y=alt.Y(value_name, stack="center", axis=None),
+			tooltip = [value_name],
+	        color="Región:N",
+	    ).properties(
+	    	title=title,
+			height=600,
+	    	width=600
+		)
+	)
+	return chart
+
 df = get_data()
 
 region = st.multiselect(
 	"Elegir regiones", list(df.columns), ["Atacama", "Ñuble", "Magallanes"]
 )
 
+start_date = st.date_input('Fecha de inicio', df.index[0])
+end_date = st.date_input('Fecha de término', df.index[-1])
+if start_date > end_date:
+    st.error('Error: La fecha de término debe ser después de la fecha de inicio.')
+
 opciones = ["Total de casos confirmados acumulados", "Total de casos confirmados acumulados por 100.000 habitantes", "Nuevos casos confirmados", "Nuevos casos confirmados por 100.000 habitantes"]
 plot = st.selectbox(
 	"Elegir gráfico", opciones
 )
-
-start_date = df.index[0]
-end_date = df.index[-1]
 
 if plot == "Nuevos casos confirmados":
 	# Agregar el 2 de marzo para hacer el diff
@@ -97,15 +130,15 @@ elif plot == "Nuevos casos confirmados":
 else:
 	st.title("Evolución de nuevos casos confirmados por 100.000 habitantes/tasa de incidencia por región")
 	
+df = df.sort_index(ascending=False)
 show_df = st.checkbox("Mostrar Tabla")
 if show_df:
 	st.write(df)
 
+df = df.sort_index(ascending=True)
 pm = st.checkbox("Promedio móvil 7 días", True)
 if pm:
 	df = df.rolling(window=7).mean()
-df = df.sort_index(ascending=False)
-
 if plot == "Total de casos confirmados acumulados":
 	title = "Total de casos confirmados acumulados*"
 elif plot == "Total de casos confirmados acumulados por 100.000 habitantes":
@@ -114,14 +147,18 @@ elif plot == "Nuevos casos confirmados":
 	title = "Nuevos casos confirmados*"
 else:
 	title = "Nuevos casos confirmados por 100.000 habitantes*"
-chart = my_altair_plot(df, "casos confirmados", title)
-st.altair_chart(chart)
 
-start_date = st.date_input('Fecha de inicio', df.index[-1])
-end_date = st.date_input('Fecha de término', df.index[0])
+option = st.radio("Tipo de gráfico", ("Normal", "Porcentaje", "Vapor"))
 
-if start_date > end_date:
-    st.error('Error: La fecha de término debe ser después de la fecha de inicio.')
+if option == "Normal":
+	chart = my_altair_plot(df, "casos confirmados", title)
+	st.altair_chart(chart)
+elif option == "Porcentaje":
+	chart = my_percentage_plot(df, "porcentaje casos confirmados", title)
+	st.altair_chart(chart)
+elif option == "Vapor":
+	chart = my_streamgraph_plot(df, "porcentaje casos confirmados", title)
+	st.altair_chart(chart)
 
 st.markdown("Autor: [Alonso Silva](https://github.com/alonsosilvaallende)")
 st.markdown("Datos: [Ministerio de Ciencia](https://github.com/MinCiencia/Datos-COVID19)")
