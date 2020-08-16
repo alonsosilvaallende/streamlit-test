2972e1e6972ec0c369ef0de44b4b1d3aaa01d346
Alonso Silva Allende <alonsosilva@gmaiil.com>
2020-08-16 15:42:04 +0200

    Add defunciones registro civil

--- app.py
+++ app.py
@@ -1,186 +1,35 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
-import altair as alt
-import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
 import datetime
-# Nombre de meses en español
-import locale
-import calendar
-_ = locale.setlocale(locale.LC_ALL, 'es_ES')
 
-# st.beta_set_page_config(
-# 	page_title="Covid-19 Chile",
-# 	page_icon="🧊",
-# 	layout="wide",
-# 	initial_sidebar_state="expanded",
-# )
+import defunciones_registro
+import casos_covid
 
-@st.cache
-def get_data():
-	URL = "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto3/CasosTotalesCumulativo_T.csv"
-	df = pd.read_csv(URL)
-	df = df.rename(columns={"Region": "fecha"})
-	df["fecha"] = pd.to_datetime(df["fecha"])
-	df = df.set_index("fecha")
-	df = df.sort_index()
-	return df
+# cd Downloads\Python\Streamlit
+# streamlit run main.py    
 
-@st.cache
-def get_population():
-    URL = "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto7/PCR.csv"
-    df = pd.read_csv(URL)
-    df = df[["Region", "Poblacion"]]
-    df = df.set_index("Region")
-    return df
-
-def my_altair_plot(df, value_name, title, option="Normal"):
-	df = df.reset_index()
-	df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name=value_name)
-	chart = (
-	    alt.Chart(df).mark_line().encode(
-	        x="fecha:T",
-	        y=alt.Y(value_name, stack=None),
-			tooltip = [value_name],
-	        color="Región:N",
-	    ).properties(
-	    	title=title,
-			height=600,
-	    	width=600
-		)
-	)
-	return chart
-	
-def my_percentage_plot(df, value_name, title):
-	df = df.reset_index()
-	df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name=value_name)
-	chart = (
-	    alt.Chart(df).mark_area().encode(
-	        x="fecha:T",
-	        y=alt.Y(value_name, axis=alt.Axis(format='%'), stack="normalize"),
-			tooltip = [value_name],
-	        color="Región:N",
-	    ).properties(
-	    	title=title,
-			height=600,
-	    	width=600
-		)
-	)
-	return chart
-
-def my_streamgraph_plot(df, value_name, title):
-	df = df.reset_index()
-	df = pd.melt(df, id_vars=["fecha"], var_name="Región" , value_name=value_name)
-	chart = (
-	    alt.Chart(df).mark_area().encode(
-	        x="fecha:T",
-	        y=alt.Y(value_name, stack="center", axis=None),
-			tooltip = [value_name],
-	        color="Región:N",
-	    ).properties(
-	    	title=title,
-			height=600,
-	    	width=600
-		)
-	)
-	return chart
-
-df = get_data()
-
-options = list(df.columns) + ["Todas las regiones"]
-region = st.sidebar.multiselect(
-	"Elegir regiones", options, ["Atacama", "Ñuble", "Magallanes"]
+# Config
+st.beta_set_page_config(
+    page_title="Covid-19 Chile",
+ 	layout="centered",
+ 	initial_sidebar_state="expanded",
 )
 
-if region[0] == "Todas las regiones":
- 	region = list(df.columns)
-
-start_date = st.sidebar.date_input('Fecha de inicio', df.index[0])
-end_date = st.sidebar.date_input('Fecha de término', df.index[-1])
-if start_date > end_date:
-    st.sidebar.error('Error: La fecha de término debe ser después de la fecha de inicio.')
-
-opciones = ["Total de casos confirmados acumulados", "Total de casos confirmados acumulados por 100.000 habitantes", "Nuevos casos confirmados", "Nuevos casos confirmados por 100.000 habitantes"]
-plot = st.selectbox(
-	"Elegir gráfico", opciones
+# Sidebar   
+st.sidebar.title('Navegación')
+opt = st.sidebar.radio("",
+    ("Casos confirmados", "Defunciones Registro Civil", "Más")
 )
 
-if plot == "Nuevos casos confirmados":
-	# Agregar el 2 de marzo para hacer el diff
-	df = df.T
-	df[pd.to_datetime("2020-03-02")] = [0 for i in range(len(df.index))]
-	df = df.T
-	df = df.sort_index()
-	df = df.diff()
-elif plot == "Total de casos confirmados acumulados por 100.000 habitantes":
-	pop = get_population()
-	df = df.T
-	pop = pop.reset_index().append({"Region": "Total", "Poblacion": pop["Poblacion"].sum()}, ignore_index=True)
-	pop = pop.set_index("Region")
-	df["Población"] = pop
-	df = df.apply(lambda x: 100000*x[:-1]/x[-1], axis=1)
-	df = df.T
-elif plot == "Nuevos casos confirmados por 100.000 habitantes":
-	# Agregar el 2 de marzo para hacer el diff
-	df = df.T
-	df[pd.to_datetime("2020-03-02")] = [0 for i in range(len(df.index))]
-	df = df.T
-	df = df.sort_index()
-	df = df.diff()
-	pop = get_population()
-	df = df.T
-	pop = pop.reset_index().append({"Region": "Total", "Poblacion": pop["Poblacion"].sum()}, ignore_index=True)
-	pop = pop.set_index("Region")
-	df["Población"] = pop
-	df = df.apply(lambda x: 100000*x[:-1]/x[-1], axis=1)
-	df = df.T
+if opt == "Defunciones Registro Civil":
+    defunciones_registro.main()
 
-df = df[region].loc[start_date:end_date]
+if opt == "Casos confirmados":
+    casos_covid.main()
 
-if plot == "Total de casos confirmados acumulados":
-	st.title("Evolución del total de casos confirmados acumulados por región")
-elif plot == "Total de casos confirmados acumulados por 100.000 habitantes":
-	st.title("Evolución del total de casos confirmados acumulados por 100.000 habitantes/tasa de incidencia acumulada por región")
-elif plot == "Nuevos casos confirmados":
-	st.title("Evolución de nuevos casos confirmados por región")
-else:
-	st.title("Evolución de nuevos casos confirmados por 100.000 habitantes/tasa de incidencia por región")
-	
-df = df.sort_index(ascending=False)
-show_df = st.checkbox("Mostrar datos")
-if show_df:
-	st.write(df)
-
-df = df.sort_index(ascending=True)
-pm = st.sidebar.checkbox("Suavizar/Promedio móvil 7 días", True)
-if pm:
-	df = df.rolling(window=7).mean()
-if plot == "Total de casos confirmados acumulados":
-	title = "Total de casos confirmados acumulados*"
-elif plot == "Total de casos confirmados acumulados por 100.000 habitantes":
-	title = "Total de casos confirmados acumulados por 100.000 habitantes*"
-elif plot == "Nuevos casos confirmados":
-	title = "Nuevos casos confirmados*"
-else:
-	title = "Nuevos casos confirmados por 100.000 habitantes*"
-
-option = st.sidebar.radio("Tipo de gráfico", ("Normal", "Porcentaje", "Vapor"))
-
-if option == "Normal":
-	chart = my_altair_plot(df, "casos confirmados", title)
-	st.altair_chart(chart)
-elif option == "Porcentaje":
-	chart = my_percentage_plot(df, "porcentaje casos confirmados", title)
-	st.altair_chart(chart)
-elif option == "Vapor":
-	chart = my_streamgraph_plot(df, "porcentaje casos confirmados", title)
-	st.altair_chart(chart)
-
-st.markdown("Autor: [Alonso Silva](https://github.com/alonsosilvaallende)")
-st.markdown("Datos: [Ministerio de Ciencia](https://github.com/MinCiencia/Datos-COVID19)")
-
-st.markdown("*El 17 de junio, se añadieron 31.422 casos confirmados debido a revisiones en el sistema de epivigilia y las fuentes de datos ([ver noticia](https://www.biobiochile.cl/noticias/nacional/chile/2020/06/16/minsal-anade-otros-31-412-contagios-covid-19-no-estaban-informados-total-supera-los-215-mil.shtml)).")
-
-if plot == "Total de casos confirmados acumulados" or plot == "Nuevos casos confirmados":
-	st.markdown("*Advertencia*: El número de casos confirmados no representa exactamente el número de casos/contagios reales. Estos últimos valores no son conocidos por límites de testeo.")
-else:
-	st.markdown("*Advertencia*: El número de casos confirmados no representa exactamente el número de casos/contagios reales. Estos últimos valores no son conocidos por límites de testeo. Además, los casos confirmados por 100.000 habitantes, dan mayores valores a regiones con pocos habitantes, por lo que pueden introducir sesgos en los datos.")
+if opt == "Más":
+    st.write("En construcción...")
---
+++ defunciones_registro.py
@@ -0,0 +1,151 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+import datetime
+
+@st.cache
+def get_regions():
+    URL = "https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto3/CasosTotalesCumulativo_T.csv"
+    df = pd.read_csv(URL, index_col="Region")
+    df = df.drop(columns="Total")
+    df = df.rename(columns={'Aysén':'Aysén del General Carlos Ibáñez del Campo',"Metropolitana": "Metropolitana de Santiago","O’Higgins":"Libertador General Bernardo O'Higgins", "Araucanía": "La Araucanía",'Magallanes':'Magallanes y de la Antártica Chilena'})
+    return list(df.columns)
+
+@st.cache
+def get_data():
+    df = pd.read_csv('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto32/Defunciones_T.csv')
+    reg = list(df.columns)
+    dicc = {}
+    for i,r in enumerate(reg):
+        if i == 0:
+            dicc[r] = 'Fecha'
+        else:
+            dicc[r] = df[r][1]
+
+    df = df.rename(dicc, axis='columns')
+    df = df.drop([0,1,2])
+    df = df.reset_index(drop=True)
+    df["Año"] = [df["Fecha"][i].split("-")[0] for i in range(df.shape[0])]
+    df["Mes"] = [df["Fecha"][i].split("-")[1] for i in range(df.shape[0])]
+    df["Dia"] = [df["Fecha"][i].split("-")[2] for i in range(df.shape[0])]
+
+    dicc_regiones = {}
+    for n, k in dicc.items():
+        reg = n.split('.')[0]
+        dicc_regiones[reg] = []
+
+    for n, k in dicc.items():
+        reg = n.split('.')[0]
+        dicc_regiones[reg].append(k)
+
+    dicc_regiones.pop("Region")
+
+    df = df[[int(df['Año'][i])>=2016 for i in range(df.shape[0])]].reset_index(drop=True)
+    l_semana = [datetime.datetime.strptime(df["Fecha"][i], '%Y-%m-%d').date().isocalendar()[1] for i in range(df.shape[0])]
+    df['Semana'] = l_semana
+
+    l_year = []
+    l_semana = []
+    l_region = []
+    l_comuna = []
+    l_def = []
+    #dfs = pd.DataFrame(columns=["Año","Semana","Region","Comuna","Defunciones"])
+    grouped = df.groupby(["Año","Semana"])
+    for name, group in grouped: 
+        for reg, comunas in dicc_regiones.items():
+            for comuna in comunas:
+                #dfs.loc[dfs.shape[0]] = [name[0],name[1],reg,comuna,sum(group[comuna])]
+                l_year.append(name[0])
+                l_semana.append(name[1])
+                l_region.append(reg)
+                l_comuna.append(comuna)
+                l_def.append(np.sum(group[comuna]))
+
+    dfs = pd.DataFrame()
+    dfs["Año"] = l_year
+    dfs["Semana"] = l_semana
+    dfs["Region"] = l_region
+    dfs["Comuna"] = l_comuna
+    dfs["Defunciones"] = l_def
+
+    return dfs
+
+def grafico_nacional(dfs):
+    fig = go.Figure()
+    colors = ["seagreen","teal","deepskyblue","gray","red"]
+    grouped = dfs.groupby("Año")
+    i = 0
+    for year, group in grouped:
+        l_semana = []
+        l_def = []
+        grouped2 = group.groupby("Semana")
+        for semana, group2 in grouped2:
+            l_semana.append(semana)
+            l_def.append(sum(group2["Defunciones"]))
+
+        fig.add_trace(go.Scatter(x=l_semana[1:-1], y=l_def[1:-1],
+                        mode='lines',
+                        name=year,
+                        marker_color=colors[i]))
+
+        i += 1
+
+    fig.update_layout(title_text='Defunciones inscritas en Chile',  xaxis_title='Número de semana')
+    return fig
+
+def grafica_region(dfs, region):
+    dfs = dfs[dfs['Region']==region]
+    fig = go.Figure()
+    colors = ["seagreen","teal","deepskyblue","gray","red"]
+    grouped = dfs.groupby("Año")
+    i = 0
+    for year, group in grouped:
+        l_semana = []
+        l_def = []
+        grouped2 = group.groupby("Semana")
+        for semana, group2 in grouped2:
+            l_semana.append(semana)
+            l_def.append(sum(group2["Defunciones"]))
+        fig.add_trace(go.Scatter(x=l_semana[1:-1], y=l_def[1:-1],
+                    mode='lines',
+                    name=year,
+                    marker_color=colors[i]))
+        i += 1
+    fig.update_layout(title_text=f'Defunciones inscritas en Región {region}',  xaxis_title='Número de semana')
+    return fig
+
+def main():
+    st.title("Defunciones inscritas Registro Civil")
+    st.write('Cantidad de defunciones inscritas en el Registro Civil por número de semana.')
+    show_df = st.checkbox('Mostrar datos')
+    df = get_data()
+    if show_df:
+        st.write(df.sort_values(by="Año"))
+
+    st.header('Gráfico Nacional')
+    fig = grafico_nacional(df)
+    st.plotly_chart(fig, use_container_width=True)
+	
+    regions = get_regions()
+    st.header('Gráfico por regiones')
+    reg = st.selectbox('Región', regions)
+    # reg = st.selectbox('Region', list(set(df['Region'])))
+    fig = grafica_region(df, reg)
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.markdown("Autor: [Joaquín Silva](https://github.com/joaquin-silva)")
+    st.markdown("Datos: [Ministerio de Ciencia](https://github.com/MinCiencia/Datos-COVID19)")
+# Sidebar
+'''
+st.sidebar.title('Navegación')
+opt = st.sidebar.radio("",
+    ("Casos", "Defunciones Registro Civil", "Más")
+)
+
+if opt == "Defunciones Registro Civil":
+    main_defunciones()
+'''
+    
