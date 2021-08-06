1792bd047786d07b1b594d5dcb2ec4ca0d0cbbc4
Alonso Silva Allende <alonsosilva@gmaiil.com>
2021-08-06 17:47:56 +0200

    Add gapminder

--- app.py
+++ app.py
@@ -1,44 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import plotly.graph_objects as go
 import plotly.express as px
-import datetime
-import calendar
-
-import defunciones_registro
-import casos_covid
-import vista_deis
-import ocupacion_hospitalaria
-import vista_icovid
-
-# cd Downloads\Python\Streamlit
-# streamlit run app.py    
-
-# Config
-st.beta_set_page_config(
-    page_title="Covid-19 Chile",
- 	layout="centered",
- 	initial_sidebar_state="expanded",
-)
-
-# Sidebar   
-st.sidebar.title('Navegación')
-opt = st.sidebar.radio("",
-    ("Casos", "Defunciones Registro Civil", "Datos Deis", "Ocupación Hospitalaria","Positivad Diaria")
-)
-
-if opt == "Defunciones Registro Civil":
-    defunciones_registro.main()
-
-if opt == "Casos":
-    casos_covid.main()
-
-if opt == "Datos Deis":
-    vista_deis.main()
-
-if opt == "Ocupación Hospitalaria":
-    ocupacion_hospitalaria.main()
-
-if opt == "Positivad Diaria":
-    vista_icovid.main()
+df = px.data.gapminder()
+fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
+           size="pop", color="continent", hover_name="country",
+           log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
+st.plotly_chart(fig)
