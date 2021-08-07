53a79a8f4a6c9e9acd784d8f19bbd4f775b00677
Alonso Silva Allende <alonsosilva@gmaiil.com>
2021-08-07 15:36:14 +0200

    Add years previous to 1996

--- app.py
+++ app.py
@@ -4,29 +4,32 @@ import pandas as pd
 import wbdata
 import plotly.express as px
 
-indicators = {'SI.POV.GINI':'Gini Index', 
-              'NY.GDP.PCAP.PP.KD':'GDP per capita (constant 2010 US$)',
-              'SP.POP.TOTL':'Population'}
-
-data = wbdata.get_dataframe(indicators=indicators)
-
-data = data.reset_index()
-
-df_region = pd.DataFrame()
-df_region["Country"]=[row['name'] for row in wbdata.get_country("")]
-df_region["Region"]=[row['region']['value'] for row in wbdata.get_country("")]
-df_region = df_region.set_index("Country")
-
-df = pd.DataFrame()
-for country in data["country"].unique():
-    if data[data["country"]==country]['Gini Index'].notna().sum() != 0 and data[data["country"]==country]['GDP per capita (constant 2010 US$)'].notna().sum() != 0:
-        df_aux = data[data["country"]==country].fillna(method="bfill").dropna()
-        df_aux["Region"]=[df_region.loc[country].values[0] for i in range(len(df_aux))]
-        df_aux=df_aux.sort_values(by="date")
-        df = pd.concat([df, df_aux], ignore_index=True)
-
-df["date"] = df["date"].astype('int64')
-df = df[df["date"]>=1996]
+@st.cache
+def get_data():
+	indicators = {'SI.POV.GINI':'Gini Index', 
+	              'NY.GDP.PCAP.PP.KD':'GDP per capita (constant 2010 US$)',
+	              'SP.POP.TOTL':'Population'}
+	
+	data = wbdata.get_dataframe(indicators=indicators)
+	
+	data = data.reset_index()
+	
+	df_region = pd.DataFrame()
+	df_region["Country"]=[row['name'] for row in wbdata.get_country("")]
+	df_region["Region"]=[row['region']['value'] for row in wbdata.get_country("")]
+	df_region = df_region.set_index("Country")
+	
+	df = pd.DataFrame()
+	for country in data["country"].unique():
+	    if data[data["country"]==country]['Gini Index'].notna().sum() != 0 and data[data["country"]==country]['GDP per capita (constant 2010 US$)'].notna().sum() != 0:
+	        df_aux = data[data["country"]==country].fillna(method="bfill").dropna()
+	        df_aux["Region"]=[df_region.loc[country].values[0] for i in range(len(df_aux))]
+	        df_aux=df_aux.sort_values(by="date")
+	        df = pd.concat([df, df_aux], ignore_index=True)
+	df["date"] = df["date"].astype('int64')
+	df = pd.concat([df[df["country"]=="Chile"],df[df["country"]!="Chile"]], ignore_index=True)
+	return df
+df = get_data()
 fig = px.scatter(df,
                 x='GDP per capita (constant 2010 US$)',
                 y='Gini Index',
@@ -36,6 +39,9 @@ fig = px.scatter(df,
                 color="Region",
                 hover_name="country",
                 log_x=True,
-                size_max=55
+                size_max=60,
+				labels={"Gini Index":"Índice de Gini",
+ "GDP per capita (constant 2010 US$)":"PIB per cápita (dólares ajustados por inflación a valor 2010) (escala log)"},
+				title="Índice de Gini vs PIB per cápita"
                 )
 st.plotly_chart(fig)
